5d16a36e8a3fbcf38c1dabba632326789bb88969
Benoît Suttor <ben.suttor@gmail.com>
2025-07-25 09:49:07 +0200

    feat(keycloak): add upgrade step to install kimug

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         "collective.z3cform.select2",
         "eea.facetednavigation",
         "pas.plugins.imio",
+        "pas.plugins.kimug",
         "imio.gdpr",
         "imio.directory.core",
         "imio.smartweb.locales",
